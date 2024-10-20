6d22c68df3f16e498b4eb94e1582c3c86e23996f
Rirfit <lilrirfit@gmail.com>
2024-10-20 11:24:50 -0300

    FC

---
+++ exercicio9/src/components/Estilo.jsx
@@ -0,0 +1,18 @@
+function Estilo(){
+    return (
+        <div>
+            <h1 style={{
+                    color: 'purple',
+                    fontSize: '24px',
+                    margin: '10px 0'
+                }} 
+            >Problemas enfrentados por crianças que descobrem de forma tardia a necessidade de usar óculos</h1>
+            <h2  style={{
+                    color: 'aqua',
+                    fontSize: '20px',
+                    margin: '10px 0'
+                }}
+                >Impactos na Saúde e Desenvolvimento Escolar de Crianças com Diagnóstico Tardio de Problemas Visuais</h2>
+        </div>
+    )
+}export default Estilo
